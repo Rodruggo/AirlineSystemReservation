1b30471cf10e5eacc12518b6a9295360eca36493
Rodruggo <araoarao.rodrigojr@cec.edu.ph>
2022-12-09 13:26:11 +0800

    first commit

---
+++ JavaApplication44/JavaApplication45/src/javaapplication45/JavaApplication45.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication45;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+/**
+ *
+ * @author user
+ */
+public class JavaApplication45 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws IOException {
+      char ch;
+          do {
+              System.out.println("Please a key followed by ENTER:");
+              ch = (char) System.in.read();
+          } while (ch != 'q');
+      }
+    }
+    
+
---
+++ JavaApplication44/src/javaapplication44/JavaApplication44.java
@@ -0,0 +1,51 @@
+
+package javaapplication44;
+import java.util.Scanner;
+public class JavaApplication44 {
+    public static void main(String[] args) {
+        
+        char operator;
+        Double number1, number2, result;
+        
+        Scanner sd = new Scanner(System.in);
+        
+        System.out.println("Choose an operator: +, -, * or /");
+        operator = sd.next().charAt(0);
+        
+        System.out.println("Enter first number: ");
+        number1 = sd.nextDouble();
+        
+        System.out.println("Enter second number: ");
+        number2 = sd.nextDouble();
+        while(operator<4){
+            System.out.println(operator);
+        }
+        
+        switch(operator){
+            case '+':
+                result = number1 + number2;
+                System.out.println(number1 + " + " + number2 + " = " +result);
+                break;
+                
+            case '-':
+                result = number1 - number2;
+                System.out.println(number1 + " - " + number2 + " = " + result);
+                break;
+                
+            case '*':
+                result = number1 * number2;
+                System.out.println(number1 + " * " + number2 + " = " + result);
+                break;
+                
+            case '/':
+                result = number1 / number2;
+                System.out.println(number1 + " / " + number2 + " = " + result);
+                break;
+                
+                default:
+                    System.out.println("Invalid Operator!");
+        }
+      sd.close();
+        
+    }
+    }
